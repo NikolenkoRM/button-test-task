3f5cdba427493650b58d5de9b01272ce07345090
Roman Nikolenko <nikolenkorm@gmail.com>
2023-11-13 12:40:13 +0100

    fix styles

--- src/app/is-viewer-disabled/is-viewer-disabled.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsViewerDisabledDirective } from './is-viewer-disabled.directive';
-
-describe('IsViewerDisabledDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IsViewerDisabledDirective();
-    expect(directive).toBeTruthy();
-  });
-});
